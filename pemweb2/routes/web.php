a689350843fe257b90307d9b873f90c57df9543f
Ariftri3 <ariftri815@gmail.com>
2025-03-19 18:12:39 +0700

    update

--- pemweb2/routes/web.php
+++ pemweb2/routes/web.php
@@ -7,6 +7,33 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/p', function () {
+    return view('web.homepage');
+});
+
+Route::get('/', function(){
+    return view('web.product');
+   });
+
+ Route::get('product/{slug}', function($slug){
+    return view('web.single_product');});
+
+    Route::get('categories', function(){
+        return view('web.categories');
+       });
+
+       Route::get('category/{slug}', function($slug){
+        return view('web.single_category');
+       });
+
+       Route::get('cart', function(){
+        return view('web.cart');
+       });
+
+       Route::get('checkout', function(){
+        return view('web.checkout');
+       });
+       
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
