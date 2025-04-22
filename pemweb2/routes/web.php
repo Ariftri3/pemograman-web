d1b332284e78e16d902174a8f114d41a2b6d445e
Ariftri3 <ariftri815@gmail.com>
2025-04-22 13:01:59 +0700

    [Database - Menambahkan tabel Products]

--- pemweb2/routes/web.php
+++ pemweb2/routes/web.php
@@ -3,36 +3,45 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+use App\Http\Controllers\HomepageController;
+
+
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/p', function () {
-    return view('web.homepage');
-});
+Route::get('/',[HomepageController::class,'index']);
+Route::get('product/{slug}', [HomepageController::class, 'product']);
+Route::get('/', [HomepageController::class, 'index']);
+Route::get('products', [HomepageController::class, 'products']);
+Route::get('product/{slug}', [HomepageController::class, 'product']);
+Route::get('categories',[HomepageController::class, 'categories']);
+Route::get('category/{slug}', [HomepageController::class, 'category']);
+Route::get('cart', [HomepageController::class, 'cart']);
+Route::get('checkout', [HomepageController::class, 'checkout']);
 
-Route::get('/', function(){
-    return view('web.product');
-   });
+// Route::get('/', function(){
+//     return view('web.product');
+//    });
 
- Route::get('product/{slug}', function($slug){
-    return view('web.single_product');});
+//  Route::get('product/{slug}', function($slug){
+//     return view('web.single_product');});
 
-    Route::get('categories', function(){
-        return view('web.categories');
-       });
+//     Route::get('categories', function(){
+//         return view('web.categories');
+//        });
 
-       Route::get('category/{slug}', function($slug){
-        return view('web.single_category');
-       });
+//        Route::get('category/{slug}', function($slug){
+//         return view('web.single_category');
+//        });
 
-       Route::get('cart', function(){
-        return view('web.cart');
-       });
+//        Route::get('cart', function(){
+//         return view('web.cart');
+//        });
 
-       Route::get('checkout', function(){
-        return view('web.checkout');
-       });
+//        Route::get('checkout', function(){
+//         return view('web.checkout');
+//        });
        
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
